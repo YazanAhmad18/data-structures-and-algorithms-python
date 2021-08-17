3bed7dab547cae4279881bda8a397744306ea0fa
yazan ahmad <yazan-ahmed1999a@outlook.com>
2021-08-17 17:09:28 +0300

    fizz buzz

--- data_structures/trees/trees/trees.py
+++ data_structures/trees/trees/trees.py
@@ -106,6 +106,34 @@ class Binary_Tree:
             arr_nodes = arr_nodes[1:]
             
         return result
+
+    def fizz_buzz_tree(self):
+        root = self.root
+        new_fizz_buzz_tree = Binary_Tree()
+
+        if not root:
+            return "tree is Empty"
+
+        def result_fizz_buzz(node):
+            if node%3==0 and node%5==0:
+                return ('FizzBuzz')
+            elif node % 3==0:
+                return ('Fizz')
+            elif node % 5 == 0:
+                return ('Buzz')
+            else:
+                return str(node)
+
+        def tree(node):
+            tree_node = Node(result_fizz_buzz(node.value))
+            if node.left:
+                tree_node.left = tree(node.left)
+            if node.right:
+                tree_node.right = tree(node.right)
+            return tree_node
+        new_fizz_buzz_tree.root = tree(root)
+
+        return (new_fizz_buzz_tree)
                 
 
 
@@ -147,3 +175,17 @@ class Binary_Search_Tree(Binary_Tree):
                     if current.right == None: 
                         return False
                     current = current.right
+
+
+if __name__ == '__main__':
+    tree = Binary_Tree()
+    tree.root=Node(1)
+    tree.root.left=Node(2)
+    tree.root.right=Node(3)
+    tree.root.left.left=Node(5)
+    tree.root.left.right=Node(7)
+    tree.root.right.right=Node(9)
+    tree.root.left.right.left=Node(15)
+    new_tree = ((tree.fizz_buzz_tree()))
+    print(new_tree.breadth_first())
+    print(tree.breadth_first())
